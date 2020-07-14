31161c6febf512b4a8a9f406f8ae6b4320167739
onkarnirhali <43208908+onkarnirhali@users.noreply.github.com>
2020-07-14 09:11:03 +0530

    Created the logic for chart & no data image added

---
+++ personal_expense_app/lib/widgets/chart.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:personal_expense_app/models/transcation.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+  Chart(this.recentTransaction);
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      var totalSum = 0.0;
+      for (var i = 0; i < recentTransaction.length; i++) {
+        if (recentTransaction[i].date.day == weekDay.day &&
+            recentTransaction[i].date.month == weekDay.month &&
+            recentTransaction[i].date.year == weekDay.year) {
+          totalSum += recentTransaction[i].amount;
+        }
+      }
+
+      print(DateFormat.E().format(weekDay));
+      print(totalSum);
+      return {'Day': DateFormat.E().format(weekDay), 'amount': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(groupedTransactions);
+    return Card(
+      elevation: 10,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
